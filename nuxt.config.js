730116d844f95b771a6715ee15c970cfd66febec
Alperen Çelik <alperen16100@gmail.com>
2022-11-10 23:20:26 +0300

    removed vite, added svg sprite + sass, ssr true

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,7 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
+  ssr: true,
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'alperencelik.com',
     htmlAttrs: {
@@ -17,35 +16,21 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['primeflex/primeflex.css'],
+  css: ['~/assets/sass/app.scss'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['nuxt-vite'],
+  buildModules: ['@nuxtjs/eslint-module'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // Doc: https://www.primefaces.org/primevue/showcase-v2/#/setup
-    'primevue/nuxt',
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+  modules: ['primevue/nuxt', '@nuxtjs/axios', '@nuxtjs/svg-sprite'],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // https://github.com/primefaces/primevue/issues/844
     transpile: ['primevue'],
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial />
+  <div>
+    <p>test</p>
+    <svg-icon name="iconPolygon" />
+    <div class="container">
+      <div class="grid">
+        <div class="col bg-black-alpha-40">asd</div>
+        <div class="col">asd</div>
+        <div class="col">asd</div>
+      </div>
+      <div class="field-checkbox">
+        <Checkbox id="binary" v-model="checked" :binary="true" />
+        <label for="binary">{{ checked }}</label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Checkbox from 'primevue/checkbox'
 export default {
   name: 'IndexPage',
+  components: {
+    Checkbox,
+  },
+  data() {
+    return {
+      checked: false,
+    }
+  },
 }
 </script>
