93f7a784d0109dc84edc7dbc637ce39aa716c6b0
Alperen Çelik <alperen16100@gmail.com>
2022-11-11 21:11:09 +0300

    added colors in tailwind config js

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,23 @@
   <div class="container">
     <p>test</p>
     <svg-icon name="iconPolygon" />
-    <div class="container">
-      <div class="grid">
-        <div v-for="n in 6" :key="n" class="col">asd</div>
-      </div>
-      <div class="flex items-center">
-        <Checkbox id="binary" v-model="checked" :binary="true" />
-        <label for="binary" class="ml-2">{{ checked }}</label>
-      </div>
-
-      <Splide :options="options">
-        <SplideSlide v-for="n in 4" :key="n">
-          <img
-            class="w-full h-52 object-cover"
-            src="https://images.unsplash.com/photo-1638204958375-4824be216720?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
-            alt="Sample 1"
-          />
-        </SplideSlide>
-      </Splide>
+    <div class="grid">
+      <div v-for="n in 6" :key="n" class="col">asd</div>
     </div>
+    <div class="flex items-center">
+      <Checkbox id="binary" v-model="checked" :binary="true" />
+      <label for="binary" class="ml-2">{{ checked }}</label>
+    </div>
+
+    <Splide :options="options">
+      <SplideSlide v-for="n in 4" :key="n">
+        <img
+          class="w-full h-52 object-cover"
+          src="https://images.unsplash.com/photo-1638204958375-4824be216720?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
+          alt="Sample 1"
+        />
+      </SplideSlide>
+    </Splide>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
       center: true,
       padding: '2rem',
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      primary: '#AC3DD3',
+      primaryLight: '#E5DAE9',
+      primaryDark: '#230A2C',
+      primaryDarken: '#190520',
+      gray: '#5A5A5A',
+      grayDark: '#4E4E4E',
+      grayLight: '#858585',
+      grayBg: '#EBEBEB',
+      grayBgLight: '#F4F4F4',
+      borderColor: '#DBDBDB',
+
+      // ...
+    },
   },
 }
